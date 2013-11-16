04c2a4d7008fbb8936024f678113f1fc99f30374
Lesly Mendez C <leslicita_mc@hotmail.com>
2013-11-16 18:21:02 -0400

    Funcionalidad ahorcado integrada a la webapp

--- src/main/java/AhorcadoServlet.java
+++ src/main/java/AhorcadoServlet.java
@@ -1,5 +1,16 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class AhorcadoServlet extends HttpServlet {
-
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String parametro = request.getParameter("letra");
+		ComandosJuego ahorcado = new ComandosJuego();
+		String letra = new String(parametro);
+		response.getWriter().println(ahorcado.verificarletra(letra));
+	}
 }
